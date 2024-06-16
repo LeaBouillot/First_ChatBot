d47329f785758f8006e28076569109d98bca2a93
bestdevelope <leabouillot7@gmail.com>
2024-06-16 16:03:28 +0200

    script TensorFlow/ reponses JS

---
+++ script.js
@@ -0,0 +1,8 @@
+const responses = {
+    "hello": "Hi there! How can I help you today?",
+    "how are you": "I'm just a bot, but I'm doing great! How about you?",
+    "what is your name": "I'm a simple chatbot created using TensorFlow.js.",
+    "default": "I'm not sure how to respond to that. Can you ask something else?",
+    "toxic": "Please keep the conversation respectful.",
+};
+
