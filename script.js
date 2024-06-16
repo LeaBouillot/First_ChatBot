d6afe34287fcd0ef08617572b82d39de53cdb150
bestdevelope <leabouillot7@gmail.com>
2024-06-16 17:45:39 +0200

    afficher reponses[inputValue]

--- script.js
+++ script.js
@@ -6,20 +6,29 @@ const responses = {
     "toxic": "Please keep the conversation respectful.",
 };
 
-const Form = document.getElementById('question');
-const input = document.getElementById('input');
+console.log(responses);
+
+const form = document.getElementById('question');
+const input = document.getElementById('text');
 const answer = document.getElementById('answer');
 
-Form.addEventListener('submit' async (e) => {
-        // On annule le comportement par defaut du formulaire HTML (donc pas de rechargement de page)
-    e.preventDefault();
-    const inputValue = input.valut.toLowerCase().trim();
-    // .toLowerCase() is used to convert all letters in a sentence to lowercase. In the second example, .trim() is used to remove any extra whitespace at the beginning or end of a string.
-    if (inputValue = '') {
-        return;
-         // Si le formulaire est vide, alors on stop la fonction, pas de resultats
+form.addEventListener('submit', (event) => {
+// Empêche le rechargement de la page
+    event.preventDefault();
+    const inputValue = input.value.toLowerCase().trim();
 
+    if (inputValue in responses) {
+        answer.innerText = responses[inputValue];
+    } else {
+        answer.innerText = responses['default'];
     }
-    
-
+    input.value = '';
+  // Si le formulaire est vide, alors on stop la fonction, pas de resultats
 })
+
+/*
+e.preventDefault(); On annule le comportement par defaut du formulaire HTML (donc pas de rechargement de page)
+.toLowerCase() is used to convert all letters in a sentence to lowercase. In the second example, 
+.trim() is used to remove any extra whitespace at the beginning or end of a string.
+
+*/
