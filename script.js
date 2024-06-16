60dc703d26f38190d5dd2601c47dff5770ff97c9
bestdevelope <leabouillot7@gmail.com>
2024-06-16 16:29:29 +0200

    form preventDefault

--- script.js
+++ script.js
@@ -6,3 +6,20 @@ const responses = {
     "toxic": "Please keep the conversation respectful.",
 };
 
+const Form = document.getElementById('question');
+const input = document.getElementById('input');
+const answer = document.getElementById('answer');
+
+Form.addEventListener('submit' async (e) => {
+        // On annule le comportement par defaut du formulaire HTML (donc pas de rechargement de page)
+    e.preventDefault();
+    const inputValue = input.valut.toLowerCase().trim();
+    // .toLowerCase() is used to convert all letters in a sentence to lowercase. In the second example, .trim() is used to remove any extra whitespace at the beginning or end of a string.
+    if (inputValue = '') {
+        return;
+         // Si le formulaire est vide, alors on stop la fonction, pas de resultats
+
+    }
+    
+
+})
